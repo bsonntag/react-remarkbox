11f086fa2aab691f1178af706bc05e8f3cdcda1e
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-04-02 20:59:52 +0100

    v0.1.1

--- dist/index.js
+++ dist/index.js
@@ -1,62 +1,79 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.default = void 0;
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _iframeResizer = require("iframe-resizer");
 
-var _iframeResizer = require('iframe-resizer');
+var _propTypes = _interopRequireDefault(require("prop-types"));
 
-var _propTypes = require('prop-types');
+var _react = _interopRequireWildcard(require("react"));
 
-var _propTypes2 = _interopRequireDefault(_propTypes);
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 function getRemarkboxUrl(remarkboxKey, threadUri) {
-  return 'https://my.remarkbox.com/embed?rb_owner_key=' + remarkboxKey + '&thread_uri=' + encodeURIComponent(threadUri);
+  return "https://my.remarkbox.com/embed?rb_owner_key=".concat(remarkboxKey, "&thread_uri=").concat(encodeURIComponent(threadUri));
 }
 
-var Remarkbox = function (_Component) {
+var Remarkbox =
+/*#__PURE__*/
+function (_Component) {
   _inherits(Remarkbox, _Component);
 
   function Remarkbox() {
-    var _ref;
+    var _getPrototypeOf2;
 
-    var _temp, _this, _ret;
+    var _this;
 
     _classCallCheck(this, Remarkbox);
 
-    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
 
-    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Remarkbox.__proto__ || Object.getPrototypeOf(Remarkbox)).call.apply(_ref, [this].concat(args))), _this), _this.iframe = null, _this.onRef = function (iframe) {
+    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(Remarkbox)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+    _defineProperty(_assertThisInitialized(_this), "iframe", null);
+
+    _defineProperty(_assertThisInitialized(_this), "onRef", function (iframe) {
       _this.iframe = iframe;
-    }, _temp), _possibleConstructorReturn(_this, _ret);
+    });
+
+    return _this;
   }
 
   _createClass(Remarkbox, [{
-    key: 'componentDidMount',
+    key: "componentDidMount",
     value: function componentDidMount() {
       if (!this.iframe) {
         return;
       }
 
       var threadFragment = this.props.threadFragment;
-
-
       (0, _iframeResizer.iframeResizer)({
         checkOrigin: ['https://my.remarkbox.com'],
         inPageLinks: true,
@@ -68,14 +85,12 @@ var Remarkbox = function (_Component) {
       }, this.iframe);
     }
   }, {
-    key: 'render',
+    key: "render",
     value: function render() {
-      var _props = this.props,
-          className = _props.className,
-          style = _props.style;
-
-
-      return _react2.default.createElement('iframe', {
+      var _this$props = this.props,
+          className = _this$props.className,
+          style = _this$props.style;
+      return _react.default.createElement("iframe", {
         className: className,
         frameBorder: 0,
         ref: this.onRef,
@@ -91,11 +106,13 @@ var Remarkbox = function (_Component) {
   return Remarkbox;
 }(_react.Component);
 
-Remarkbox.propTypes = {
-  className: _propTypes2.default.string,
-  remarkboxKey: _propTypes2.default.string.isRequired,
-  style: _propTypes2.default.object,
-  threadFragment: _propTypes2.default.string,
-  threadUri: _propTypes2.default.string.isRequired
-};
-exports.default = Remarkbox;
+_defineProperty(Remarkbox, "propTypes", {
+  className: _propTypes.default.string,
+  remarkboxKey: _propTypes.default.string.isRequired,
+  style: _propTypes.default.object,
+  threadFragment: _propTypes.default.string,
+  threadUri: _propTypes.default.string.isRequired
+});
+
+var _default = Remarkbox;
+exports.default = _default;
